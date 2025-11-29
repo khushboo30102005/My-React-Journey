403503179f8e433f710528d16a68ae79c2af39e9
khushboo saini <khushboo30102005@gmail.com>
2025-11-29 20:05:42 +0530

    explore components

---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  const name = "My React Journey"
+  return (
+    <div id='card'>
+      <h1>{name}</h1>
+      <p>A simple repo to track my React learning progress — concepts, notes, and small practice projects.  
+Learning React step by step with consistency and clean code.</p>
+    </div>
+  )
+}
+
+export default Card
