c7c73fc5855b16e1ae50e41ef4aaf0ddc8cd812a
khushboo saini <khushboo30102005@gmail.com>
2025-12-04 22:08:55 +0530

    tailwind setup

---
+++ 06-Tailwind/src/App.jsx
@@ -0,0 +1,8 @@
+export  const App = () => {
+  return (
+    <div className="h-screen w-full bg-gray-300">
+      <h2 className="bg-amber-300 font-bold p-4">Tailwind</h2>
+    </div>
+  )
+}
+export default App
